b09f66eee4211951ebe6039f14bd5e9f5d793bc7
Mahmoud Abbas <mahmoud.abbas@pwc.com>
2023-09-12 13:26:15 +0300

    Login Page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,78 @@
+import { Container, Flex, Image, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import Gmail from "../assests/images/gmail.png";
+import Outlook from "../assests/images/outlook.png";
+import ICloud from "../assests/images/icloud.png";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [token, setToken] = useState("");
+  const navigate = useNavigate();
+
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      setToken(tokenResponse.access_token);
+    },
+    scope: "https://www.googleapis.com/auth/gmail.readonly",
+  });
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+      navigate("/");
+    }
+  }, [navigate, token]);
+  return (
+    <Container
+      maxW="full"
+      h="100vh"
+      display="flex"
+      flexDir="column"
+      justifyContent="center"
+      alignItems="center"
+      backgroundColor="#E9EEFF"
+      gap="20px"
+    >
+      <Flex
+        backgroundColor="#fff"
+        alignItems="center"
+        p="10px"
+        rounded="20px"
+        gap="10px"
+        cursor="pointer"
+        w="250px"
+        onClick={login}
+      >
+        <Image src={Gmail} w="50px" h="50px" />
+        <Text fontWeight="600">Connect with Gmail</Text>
+      </Flex>
+      <Flex
+        backgroundColor="#fff"
+        alignItems="center"
+        p="10px"
+        rounded="20px"
+        gap="10px"
+        cursor="pointer"
+        w="250px"
+      >
+        <Image src={Outlook} w="50px" h="50px" />
+        <Text fontWeight="600">Connect with Outlook</Text>
+      </Flex>
+      <Flex
+        backgroundColor="#fff"
+        alignItems="center"
+        p="10px"
+        rounded="20px"
+        gap="10px"
+        cursor="pointer"
+        w="250px"
+      >
+        <Image src={ICloud} w="50px" h="50px" />
+        <Text fontWeight="600">Connect with ICloud</Text>
+      </Flex>
+    </Container>
+  );
+}
+
+export default Login;
