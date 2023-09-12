ddf267c4737e634a41c68422be587c8b7b80d172
Mahmoud Abbas <mahmoud.abbas@pwc.com>
2023-09-12 13:26:03 +0300

    Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { Button, Flex, Image, Input, Text } from "@chakra-ui/react";
+import Edit from "../assests/images/edit.png";
+
+function Header({ textInput, setTextInput }) {
+  return (
+    <>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Text fontSize="25px">All Messages</Text>
+        <Button
+          border="unset"
+          backgroundColor="#4D77FF"
+          h="30px"
+          w="50px"
+          cursor="pointer"
+        >
+          <Image src={Edit} w="20px" />
+        </Button>
+      </Flex>
+      <Input
+        h="40px"
+        px="10px"
+        fontSize="15px"
+        placeholder="Search Message"
+        border="1px solid #ccc"
+        bg="#fff"
+        value={textInput}
+        onChange={(e) => setTextInput(e.target.value)}
+      />
+    </>
+  );
+}
+
+export default Header;
